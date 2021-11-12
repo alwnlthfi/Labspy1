05ebba276aa8fe72a689ff19269057f049c98dd7
alwnlthfi <alwanluthfi100@gmail.com>
2021-11-12 23:20:23 +0700

    menambahkan file ss

---
+++ program/lat 1.py
@@ -0,0 +1,12 @@
+print()
+print(" Mencari Bilangan Terbesar")
+print("--------------------------")
+
+a=int(input("Masukan Bilangan A :"))
+b=int(input("Masukan Bilangan B :"))
+print()
+
+if a>b :
+    print("Bilangan Terbesar Adalah A")
+elif b>a :
+    print("Bilangan Terbesar Adalah B")
